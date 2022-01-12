786c7aaadab25a166c9e93795bb715c14c28cec9
unknown <chepurnojv@gmail.com>
2022-01-12 20:37:27 +0200

    colors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const colors = require("colors");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -26,12 +27,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} node on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} node on port ${PORT}`.yellow.bold
+  )
 );
 
 //handle unhendled reje
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   //close server exit process
   server.close(() => process.exit(1));
 });
