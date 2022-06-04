795bf16fce7a5805ac11eab8abff9151fe37a13c
unknown <chepurnojv@gmail.com>
2022-06-04 12:34:38 +0300

    complete server side app

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -46,6 +49,20 @@ app.use(helmet());
 
 //prevent xss attacks
 app.use(xss());
+
+//rate limiter
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+//prevent http param polution
+app.use(hpp());
+
+//enable cors
+app.use(cors());
 
 //set static folder
 app.use(express.static(path.join(__dirname, "public")));
